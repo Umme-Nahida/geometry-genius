66e685aa0e648f29dc32a9b0c5cb97fe9982a003
Umme-Nahida <mysuccessmission@gmail.com>
2024-04-18 19:57:30 +0600

    set triangle area

--- script/area.calculator.js
+++ script/area.calculator.js
@@ -2,10 +2,18 @@ function calculateTriangle(){
     const baseField = document.getElementById("triangle-base");
     const baseValueText = baseField.value;
     const base = parseFloat(baseValueText);
+    baseField.value ="";
     
     // height field
     const heightField = document.getElementById("triangle-height");
     const heightValue = heightField.value;
     const height = parseFloat(heightValue)
-    console.log(base, height)
+    heightField.value ="";
+    
+    const area = 0.5 * base * height;
+    console.log(area)
+    // get triangle area
+    const triangleArea = document.getElementById("triangle-area");
+    triangleArea.innerText = area;
+
 }
